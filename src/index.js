1d308eda4895fc6b658b651859574ef58db0fea4
Ilya Khinevich <ilyaxinevich@gmail.com>
2021-03-14 15:24:12 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
+  let one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let two = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   
+  let num = String(number).split('');
+
+  if (number === 0)
+    return 'zero';
+  else if (number < 20)
+    return one[number];
+  else if (number < 100)
+    return (two[num[0]] + ' ' + one[num[1]]).trim();
+  else if (number < num[0]+20)
+    return (one[num[0]] + ' hundred ' + one[Number(num[1] + num[2])]).trim();
+  else if (number < 1000)
+    return (one[num[0]] + ' hundred ' + two[num[1]] + ' ' + one[num[2]]).trim();
+
+
 }
